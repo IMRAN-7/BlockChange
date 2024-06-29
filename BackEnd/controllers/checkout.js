06b7156728274a3c8ff148c8732e0c9980ea9235
IMRAN-7 <mirzamohammadimran786@gmail.com>
2024-06-29 22:16:36 +0530

    Updation of link

--- BackEnd/controllers/checkout.js
+++ BackEnd/controllers/checkout.js
@@ -3,4 +3,23 @@ import crypto from "crypto"
 import {Volunteer} from "../models/volunteer.js"
 
 //Base URL
-const cryptomus=axios.create({baseURL:""})
+const cryptomus=axios.create({baseURL:"https://api.cryptomus.com/v1"})
+
+const createInvoice =async(amount)=>{
+    try{
+       const data={
+        amount:amount,
+        currency:"USD",
+        order_id:crypto.randomBytes(12).toString("hex"),
+        url_return:"https://block-change-qjme.vercel.app/donate",
+        url_success:"https://block-change-qjme.vercel.app/",
+        lifetime:300,
+    };
+    const sign=crypto.createHash("md5").update(Buffer.from(JSON.stringify(data)).toString("base64")+process.env.PAYMENT_API_KEY)
+    
+ } catch(error)
+    {
+
+    }
+
+}
