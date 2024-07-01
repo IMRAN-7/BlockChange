755818322a8e82439e3c1fba23da9273f05a23a3
IMRAN-7 <mirzamohammadimran786@gmail.com>
2024-07-01 12:35:33 +0530

    Updation

--- BackEnd/controllers/checkout.js
+++ BackEnd/controllers/checkout.js
@@ -2,6 +2,26 @@ import axios from "axios"
 import crypto from "crypto"
 import {Volunteer} from "../models/volunteer.js"
 
+export const checkout=async(req,res)=>{
+    try{
+       const {amount}=req.body;
+       const invoice=await createInvoice(amount);
+       await Volunteer.create({
+        ...req.body, orderId:invoice.result.orderId, paymentStatus:invoice.result.paymentstatus,
+       })
+       res.send(invoice);
+       console.log(amount)
+    }
+    catch(error)
+    {
+     console.log(error);
+     res.status(500).json({
+        success:false,
+        message:"Internal Server Error"
+     })
+    }
+}
+
 //Base URL
 const cryptomus=axios.create({baseURL:"https://api.cryptomus.com/v1"})
 
@@ -16,10 +36,20 @@ const createInvoice =async(amount)=>{
         lifetime:300,
     };
     const sign=crypto.createHash("md5").update(Buffer.from(JSON.stringify(data)).toString("base64")+process.env.PAYMENT_API_KEY)
+    .digest("hex");
     
+    const headers={
+        merchant:process.env.MERCHANT_ID,
+        sign,
+    };
+
+    const response=await cryptomus.post("/payment",data,{headers});
+    return response.data;
+
  } catch(error)
     {
-
+        console.error("Error Occured:",error);
+        throw error;
     }
 
 }
